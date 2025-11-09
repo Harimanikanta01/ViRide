cd0fe4ae661d1d85bbcbb3eb2fc3c0dc8b3b2643
Harimanikanta01 <punugulahari1>
2025-11-09 17:08:46 +0530

    added cors

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express=require("express")
 const mongoose=require("mongoose")
 require('dotenv').config();
-
+const cors=require("cors")
 const app=express()
+app.use(cors())
 const authentication=require("./routers/Authentication")
 const router1=require("./routers/mainlogic")
 app.use(express.json())
